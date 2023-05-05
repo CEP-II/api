cb080f3cd2fdc194ff07f3f21b7ff74c7484a7c8
Nestor <nikolajkjakobsen@gmail.com>
2023-05-05 14:43:49 +0200

    3rd time's the charm

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const checkAuth = require('../middleware/checkAuth')
 const authorizeRoles = require('../middleware/authorizeRoles')
 
 const adminController = require('../controller/admin')

e3ddfb8372d1ddfb2dba732da449bdb12125fcad
Nestor <nikolajkjakobsen@gmail.com>
2023-05-10 17:34:44 +0200

    Phone stuff

--- api/controller/accident.js
+++ api/controller/accident.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose')
 const Accident = require('../models/accident')
 const Citizen = require('../models/citizen')
 
+// SMS STUFF
+const twilio = require('twilio')
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const numberFrom = process.env.TWILIO_PHONE_NUMBER;
+const numberTo = process.env.TWILIO_NUMBER_TO_SEND_TO;
+
 exports.report_accident = (req, res, next) => {
     // find citizen id from device id.
     Citizen.findOne({deviceId: req.body.deviceId})
@@ -19,6 +27,16 @@ exports.report_accident = (req, res, next) => {
                 positionId:  req.body.positionId,
                 alarmTime: req.body.alarmTime,
             })
+
+            const client = new twilio(accountSid, authToken)
+
+            // Send SMS using Twilio, this should send an sms
+            client.messages.create({
+                body: `Accident reported by citizen ${citizen._id}. Device ID: ${accident.deviceId}, Position ID: ${accident.positionId}, Alarm Time: ${accident.alarmTime}`,
+                to: numberTo,
+                from: numberFrom, // Your Twilio phone number
+            })
+            .then(message => console.log(message.sid));
 
             // Should check that the citizen id is valid (in DB)? Or is this waste of ressources
             accident.save()

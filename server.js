52003144f3804048ab1073e7f1eeafaf45605df2
Nestor <nikolajkjakobsen@gmail.com>
2023-05-13 18:51:54 +0200

    change to port 80 instead of 3000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require("./app");
 // unfortunately neither of us have a domain so we will probably just run this on localhost and
 // expose this as a virtual domain through ngrok or something similar.
 
-const port = process.env.port || 3000;
+const port = process.env.port || 80;
 
 // listener: function execute whenver we get new request, returns the response
 const server = http.createServer(app);
